480d0c26a96ed4c577ea43314e5d59195cc18ae2
Moderocky <admin@moderocky.com>
2023-10-05 14:28:17 +0100

    Add syntax root.

---
+++ src/main/java/mx/kenzie/skriptlab/Syntax.java
@@ -0,0 +1,21 @@
+package mx.kenzie.skriptlab;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Condition;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Section;
+import ch.njol.skript.lang.TriggerItem;
+
+public record Syntax(Class<? extends TriggerItem> type, String... patterns) {
+    
+    @SuppressWarnings("unchecked")
+    public void register() {
+        if (Section.class.isAssignableFrom(type))
+            Skript.registerSection((Class<? extends Section>) type, patterns);
+        else if (Effect.class.isAssignableFrom(type))
+            Skript.registerEffect((Class<? extends Effect>) type, patterns);
+        else if (Condition.class.isAssignableFrom(type))
+            Skript.registerCondition((Class<? extends Condition>) type, patterns);
+    }
+    
+}
