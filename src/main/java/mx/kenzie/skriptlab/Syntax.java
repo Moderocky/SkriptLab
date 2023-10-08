7242d9927e5358c9454f029afa224ae0320937c0
Moderocky <admin@moderocky.com>
2023-10-08 11:02:54 +0100

    Add expression registration support.

--- src/main/java/mx/kenzie/skriptlab/Syntax.java
+++ src/main/java/mx/kenzie/skriptlab/Syntax.java
@@ -1,12 +1,12 @@
 package mx.kenzie.skriptlab;
 
 import ch.njol.skript.Skript;
-import ch.njol.skript.lang.Condition;
-import ch.njol.skript.lang.Effect;
-import ch.njol.skript.lang.Section;
-import ch.njol.skript.lang.SyntaxElement;
+import ch.njol.skript.lang.*;
+import mx.kenzie.skriptlab.template.DirectExpression;
 
-public record Syntax(Class<? extends SyntaxElement> type, String... patterns) {
+public record Syntax
+    <Handler extends SyntaxElement>
+    (Class<? extends SyntaxElement> type, Handler handler, String... patterns) {
     
     @SuppressWarnings("unchecked")
     public void register() {
@@ -16,6 +16,22 @@ public record Syntax(Class<? extends SyntaxElement> type, String... patterns) {
             Skript.registerEffect((Class<? extends Effect>) type, patterns);
         else if (Condition.class.isAssignableFrom(type))
             Skript.registerCondition((Class<? extends Condition>) type, patterns);
+        else if (Expression.class.isAssignableFrom(type))
+            this.registerExpression(this.holderType(), this.expressionReturnType(), patterns);
+    }
+    
+    private <Holder extends Expression<Type>, Type> void registerExpression(Class<Holder> holder, Class<Type> type, String... patterns) {
+        Skript.registerExpression(holder, type, ExpressionType.SIMPLE, patterns);
+    }
+    
+    @SuppressWarnings("unchecked")
+    <Type extends SyntaxElement> Class<Type> holderType() {
+        return (Class<Type>) type;
+    }
+    
+    @SuppressWarnings("unchecked")
+    <Type> Class<Type> expressionReturnType() {
+        return handler instanceof DirectExpression<?> expression ? (Class<Type>) expression.getReturnType() : null;
     }
     
 }
